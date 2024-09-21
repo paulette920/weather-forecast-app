65b8fa79e9945f09c60adbca77244410ddde4e03
Dushime Paulette <p.dushime12@gmail.com>
2024-09-21 15:56:31 +0200

    Added another file of js and added some basic js to the code

---
+++ src/script.js
@@ -0,0 +1,13 @@
+function SearchCity(event) {
+  //preventing the normal behaviour of a form
+  event.preventDefault();
+  //selecting the input form and accesing the input of the user and storing it in a variable of city
+  let SearchCityElement = document.querySelector("#search-form");
+  let cityElement = document.querySelector("#city");
+  let city = SearchCityElement.value;
+  cityElement.innerHTML = city;
+}
+
+//selecting the whole form
+let SearchForm = document.querySelector("#form");
+SearchForm.addEventListener("submit", SearchCity);
