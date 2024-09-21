04f42b91ae52c7856941478c93b9f524b608aa5f
Dushime Paulette <p.dushime12@gmail.com>
2024-09-21 18:20:46 +0200

    Added the functionality of the actual weather in different cities

--- src/script.js
+++ src/script.js
@@ -1,13 +1,28 @@
+function UpdateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature-value");
+  let degrees = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+  let city = response.data.city;
+  temperatureElement.innerHTML = degrees;
+  cityElement.innerHTML = city;
+}
+
+function SearchCity(city) {
+  let apiKey = "409358oa0b2d74cc98fa66aabc1789t2";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(UpdateWeather);
+}
 function HandleSubmit(event) {
   //preventing the normal behaviour of a form
   event.preventDefault();
   //selecting the input form and accesing the input of the user and storing it in a variable of city
   let SearchCityElement = document.querySelector("#search-form");
-  let cityElement = document.querySelector("#city");
   let city = SearchCityElement.value;
-  cityElement.innerHTML = city;
+  SearchCity(city);
 }
 
 //selecting the whole form
 let SearchForm = document.querySelector("#form");
 SearchForm.addEventListener("submit", HandleSubmit);
+
+SearchCity("Rwanda");
